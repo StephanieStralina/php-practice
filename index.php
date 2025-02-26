525996c9edd360884574d538c2ff9295d076b55a
StephanieStralina <StephanieDRichter@gmail.com>
2025-02-26 12:36:08 -0600

    PHP Floats

--- index.php
+++ index.php
@@ -26,4 +26,17 @@ is_bool($isComplete);
 /* INTEGERS */
 
 $x = 5;
-//can do octonumbers, binary numbers, etc.
+//can do base 10 numbers, octonumbers, binary numbers, etc.
+//can run PHP_INT_MAX to see absolute max number PHP can work with
+//$x = (int) 5.98 it will drop the int (not round)
+
+/* FLOATS */
+
+$y = 13.5;
+//can use e3/e-3 
+//PHP_FLOAT_MAX/_MIN upper limits of float
+//floor rounds down, ceil rounds up
+//don't compare floats directly x == y
+// is_nan(log(-1)) == false
+
+echo $y;
